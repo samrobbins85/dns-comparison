6029ae2683bcbf8aebef6b0ff36f621e3345f2de
Sam Robbins <samrobbinsgb@gmail.com>
2020-07-15 14:06:17 +0100

    Start work on bulk upload

--- pages/bulk.js
+++ pages/bulk.js
@@ -1,3 +1,37 @@
+import React, { useState } from "react";
+
 export default function Bulk() {
-  return <h1>Hello, World</h1>;
+  const [file, setFile] = useState([""]);
+
+  function FileChange(event) {
+    var file = event.target.files[0];
+    var reader = new FileReader();
+    reader.onload = function (event) {
+      console.log("First log");
+      setFile(event.target.result.split(/r?\n/));
+      console.log(event.target.result.split(/r?\n/));
+    };
+    // console.log("Second log");
+    reader.readAsText(file);
+  }
+
+  return (
+    <div className="bg-white flex flex-col h-screen">
+      <main className="flex-grow mx-auto p-8">
+        <h1 className="title text-center text-5xl font-bold">DNS Comparison</h1>
+
+        <p className="description text-center text-xl text-gray-700 mb-6">
+          Upload a URL using the button below
+        </p>
+        <form className="text-center flex justify-center">
+          <input type="file" id="input" onChange={FileChange} />
+        </form>
+        <ul>
+          {file.map((domain) => (
+            <li key={domain}>{domain}</li>
+          ))}
+        </ul>
+      </main>
+    </div>
+  );
 }
