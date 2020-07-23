e954e1892c2d95d85a8ef3f315807250e4c33519
Sam Robbins <samrobbinsgb@gmail.com>
2020-07-23 20:12:06 +0100

    ESLint

--- pages/bulk.js
+++ pages/bulk.js
@@ -6,175 +6,181 @@ import axios from "axios";
 import { Bar } from "react-chartjs-2";
 
 export default function Bulk() {
-  const [file, setFile] = useState([""]);
-  const [cf, setCF] = useState([""]);
-  const [chart, setChart] = useState({
-    labels: ["Cloudflare", "Google", "Quad9"],
+	const [file, setFile] = useState([""]);
+	const [cf, setCF] = useState([""]);
+	const [chart, setChart] = useState({
+		labels: ["Cloudflare", "Google", "Quad9"],
 
-    datasets: [
-      {
-        label: "Blocks",
-        backgroundColor: "rgba(255,99,132,0.2)",
-        borderColor: "rgba(255,99,132,1)",
-        borderWidth: 1,
-        hoverBackgroundColor: "rgba(255,99,132,0.4)",
-        hoverBorderColor: "rgba(255,99,132,1)",
-        data: [1, 1, 1],
-      },
-    ],
-  });
+		datasets: [
+			{
+				label: "Blocks",
+				backgroundColor: "rgba(255,99,132,0.2)",
+				borderColor: "rgba(255,99,132,1)",
+				borderWidth: 1,
+				hoverBackgroundColor: "rgba(255,99,132,0.4)",
+				hoverBorderColor: "rgba(255,99,132,1)",
+				data: [0, 0, 0],
+			},
+		],
+	});
 
-  useEffect(() => {
-    var requests = [];
-    file.forEach((domain) => {
-      requests.push(
-        axios.get(
-          `https://Cloudflare-dns.com/dns-query?ct=application/dns-json&type=AAAA&name=${domain}`
-        ),
-        axios.get(`https://dns.google/resolve?name=${domain}`),
-        axios.get(`https://dns.quad9.net:5053/dns-query?name=${domain}`)
-      );
-    });
-    const fetchData = async () => {
-      axios
-        .all(requests)
-        .then(
-          axios.spread((...responses) => {
-            var data = [];
-            var i;
-            var quad1 = 0;
-            var quad8 = 0;
-            var quad9 = 0;
-            for (i = 0; i < responses.length; i += 3) {
-              if (responses[i].data.Status !== 0) {
-                quad1++;
-              }
-              if (responses[i + 1].data.Status !== 0) {
-                quad8++;
-              }
-              if (responses[i + 2].data.Status !== 0) {
-                quad9++;
-              }
-              data.push({
-                domain: file[i / 3],
-                cloudflare: responses[i].data.Status,
-                google: responses[i + 1].data.Status,
-                Quad9: responses[i + 2].data.Status,
-              });
-            }
-            // setBlock1(quad1);
-            // setBlock8(quad8);
-            // setBlock9(quad9);
-            setCF(data);
-            setChart({
-              labels: ["Cloudflare", "Google", "Quad9"],
-              datasets: [
-                {
-                  label: "Blocks",
-                  backgroundColor: "rgba(255,99,132,0.2)",
-                  borderColor: "rgba(255,99,132,1)",
-                  borderWidth: 1,
-                  hoverBackgroundColor: "rgba(255,99,132,0.4)",
-                  hoverBorderColor: "rgba(255,99,132,1)",
-                  data: [quad1, quad8, quad9],
-                },
-              ],
-            });
-          })
-        )
-        .catch((errors) => {
-          setCF([]);
-        });
-    };
-    console.log("Start fetching data");
-    fetchData();
-    console.log(cf);
-  }, [file]);
+	useEffect(() => {
+		var requests = [];
+		file.forEach((domain) => {
+			requests.push(
+				axios.get(
+					`https://Cloudflare-dns.com/dns-query?ct=application/dns-json&type=AAAA&name=${domain}`
+				),
+				axios.get(`https://dns.google/resolve?name=${domain}`),
+				axios.get(`https://dns.quad9.net:5053/dns-query?name=${domain}`)
+			);
+		});
+		const fetchData = async () => {
+			axios
+				.all(requests)
+				.then(
+					axios.spread((...responses) => {
+						var data = [];
+						var i;
+						var quad1 = 0;
+						var quad8 = 0;
+						var quad9 = 0;
+						for (i = 0; i < responses.length; i += 3) {
+							if (responses[i].data.Status !== 0) {
+								quad1++;
+							}
+							if (responses[i + 1].data.Status !== 0) {
+								quad8++;
+							}
+							if (responses[i + 2].data.Status !== 0) {
+								quad9++;
+							}
+							data.push({
+								domain: file[i / 3],
+								cloudflare: responses[i].data.Status,
+								google: responses[i + 1].data.Status,
+								Quad9: responses[i + 2].data.Status,
+							});
+						}
+						// setBlock1(quad1);
+						// setBlock8(quad8);
+						// setBlock9(quad9);
+						setCF(data);
+						setChart({
+							labels: ["Cloudflare", "Google", "Quad9"],
+							datasets: [
+								{
+									label: "Blocks",
+									backgroundColor: "rgba(255,99,132,0.2)",
+									borderColor: "rgba(255,99,132,1)",
+									borderWidth: 1,
+									hoverBackgroundColor:
+										"rgba(255,99,132,0.4)",
+									hoverBorderColor: "rgba(255,99,132,1)",
+									data: [quad1, quad8, quad9],
+								},
+							],
+						});
+					})
+				)
+				.catch(() => {
+					setCF([]);
+				});
+		};
+		console.log("Start fetching data");
+		fetchData();
+		console.log(cf);
+	}, [file]);
 
-  const columns = React.useMemo(
-    () => [
-      {
-        Header: "Domain",
+	const columns = React.useMemo(
+		() => [
+			{
+				Header: "Domain",
 
-        accessor: "domain", // accessor is the "key" in the data
-      },
+				accessor: "domain", // accessor is the "key" in the data
+			},
 
-      {
-        Header: "Cloudflare",
+			{
+				Header: "Cloudflare",
 
-        accessor: "cloudflare",
-      },
+				accessor: "cloudflare",
+			},
 
-      {
-        Header: "Google",
+			{
+				Header: "Google",
 
-        accessor: "google",
-      },
+				accessor: "google",
+			},
 
-      {
-        Header: "Quad9",
+			{
+				Header: "Quad9",
 
-        accessor: "Quad9",
-      },
-    ],
+				accessor: "Quad9",
+			},
+		],
 
-    []
-  );
+		[]
+	);
 
-  function FileChange(event) {
-    var file = event.target.files[0];
-    var reader = new FileReader();
-    reader.onload = function (event) {
-      setFile(
-        event.target.result.split(/r?\n/).filter(function (e) {
-          return e;
-        })
-      );
-      console.log(event.target.result.split(/r?\n/));
-    };
-    reader.readAsText(file);
-  }
+	function FileChange(event) {
+		var file = event.target.files[0];
+		var reader = new FileReader();
+		reader.onload = function (event) {
+			setFile(
+				event.target.result.split(/r?\n/).filter(function (e) {
+					return e;
+				})
+			);
+			console.log(event.target.result.split(/r?\n/));
+		};
+		reader.readAsText(file);
+	}
 
-  const scale = {
-    scales: {
-      yAxes: [
-        {
-          ticks: {
-            beginAtZero: true,
-          },
-        },
-      ],
-    },
-  };
-  return (
-    <>
-      <Head>
-        <title>Bulk Upload | DNS Comparison</title>
-        <link
-          rel="icon"
-          href="https://www.globalcyberalliance.org/wp-content/uploads/favicon.png"
-        />
-      </Head>
-      <div className="flex flex-col h-screen bg-white">
-        <NavBar />
-        <main className="flex-grow p-8">
-          <h1 className="text-5xl font-bold text-center title">
-            DNS Comparison
-          </h1>
+	const scale = {
+		scales: {
+			yAxes: [
+				{
+					ticks: {
+						beginAtZero: true,
+					},
+				},
+			],
+		},
+	};
+	return (
+		<>
+			<Head>
+				<title>Bulk Upload | DNS Comparison</title>
+				<link
+					rel="icon"
+					href="https://www.globalcyberalliance.org/wp-content/uploads/favicon.png"
+				/>
+			</Head>
+			<div className="flex flex-col h-screen bg-white">
+				<NavBar />
+				<main className="flex-grow p-8">
+					<h1 className="text-5xl font-bold text-center title">
+						DNS Comparison
+					</h1>
 
-          <p className="mb-6 text-xl text-center text-gray-700 description">
-            Upload a File using the button below
-          </p>
-          <form className="flex justify-center text-center">
-            <input type="file" id="input" onChange={FileChange} />
-          </form>
-          <div>
-            <h2>Bar Example (custom size)</h2>
-            <Bar data={chart} width={100} height={20} options={scale} />
-          </div>
-          <Table columns={columns} data={cf} />
-        </main>
-      </div>
-    </>
-  );
+					<p className="mb-6 text-xl text-center text-gray-700 description">
+						Upload a File using the button below
+					</p>
+					<form className="flex justify-center text-center">
+						<input type="file" id="input" onChange={FileChange} />
+					</form>
+					<div>
+						<h2>Number of blocks comparison</h2>
+						<Bar
+							data={chart}
+							width={100}
+							height={20}
+							options={scale}
+						/>
+					</div>
+					<Table columns={columns} data={cf} />
+				</main>
+			</div>
+		</>
+	);
 }
--- pages/index.js
+++ pages/index.js
@@ -2,107 +2,119 @@ import Head from "next/head";
 import React, { useState, useEffect } from "react";
 import NavBar from "../components/navbar";
 import axios from "axios";
+import PropTypes from "prop-types";
 export default function Home() {
-  const [domain, setDomain] = useState("");
-  const [result, setResult] = useState("");
+	const [domain, setDomain] = useState("");
+	const [result, setResult] = useState("");
+	function handleChange(event) {
+		var domain;
+		try {
+			domain = new URL(event.target.value).hostname;
+		} catch {
+			try {
+				domain = new URL("https://" + event.target.value).hostname;
+			} catch {
+				domain = event.target.value;
+			}
+		}
+		setDomain(domain);
+	}
 
-  function handleChange(event) {
-    var domain;
-    try {
-      domain = new URL(event.target.value).hostname;
-    } catch {
-      try {
-        domain = new URL("https://" + event.target.value).hostname;
-      } catch {
-        domain = event.target.value;
-      }
-    }
-    setDomain(domain);
-  }
+	function Provider(props) {
+		var icon;
+		if (result[props.index] === 0) {
+			icon = "/available.svg";
+		} else {
+			icon = "/not_available.svg";
+		}
+		return (
+			<>
+				<div className="grid grid-cols-2 gap-4 pt-6">
+					<div className="flex justify-center">
+						<img
+							className="h-12"
+							src={props.logo}
+							alt="Company Logo"
+						/>
+					</div>
+					<div className="flex justify-center text-center">
+						<img src={icon} alt="status" />
+					</div>
+				</div>
+				<hr className="mt-4" />
+			</>
+		);
+	}
+	Provider.propTypes = {
+		index: PropTypes.string.isRequired,
+		logo: PropTypes.string.isRequired,
+	};
 
-  function Provider(props) {
-    var icon;
-    if (result[props.index] === 0) {
-      icon = "/available.svg";
-    } else {
-      icon = "/not_available.svg";
-    }
-    return (
-      <>
-        <div className="grid grid-cols-2 gap-4 pt-6">
-          <div className="flex justify-center">
-            <img className="h-12" src={props.logo} alt="Company Logo" />
-          </div>
-          <div className="flex justify-center text-center">
-            <img src={icon} alt="status" />
-          </div>
-        </div>
-        <hr className="mt-4" />
-      </>
-    );
-  }
+	useEffect(() => {
+		const cloudflare = axios.get(
+			`https://Cloudflare-dns.com/dns-query?ct=application/dns-json&type=AAAA&name=${domain}`
+		);
+		const google = axios.get(`https://dns.google/resolve?name=${domain}`);
+		const quad9 = axios.get(
+			`https://dns.quad9.net:5053/dns-query?name=${domain}`
+		);
+		const fetchData = async () => {
+			axios
+				.all([cloudflare, google, quad9])
+				.then(
+					axios.spread((...responses) => {
+						const responseOne = responses[0];
+						const responseTwo = responses[1];
+						const responesThree = responses[2];
+						setResult([
+							responseOne.data.Status,
+							responseTwo.data.Status,
+							responesThree.data.Status,
+						]);
+					})
+				)
+				.catch(() => {
+					setResult([-1, -1, -1]);
+				});
+		};
 
-  useEffect(() => {
-    const cloudflare = axios.get(
-      `https://Cloudflare-dns.com/dns-query?ct=application/dns-json&type=AAAA&name=${domain}`
-    );
-    const google = axios.get(`https://dns.google/resolve?name=${domain}`);
-    const quad9 = axios.get(
-      `https://dns.quad9.net:5053/dns-query?name=${domain}`
-    );
-    const fetchData = async () => {
-      axios
-        .all([cloudflare, google, quad9])
-        .then(
-          axios.spread((...responses) => {
-            const responseOne = responses[0];
-            const responseTwo = responses[1];
-            const responesThree = responses[2];
-            setResult([
-              responseOne.data.Status,
-              responseTwo.data.Status,
-              responesThree.data.Status,
-            ]);
-          })
-        )
-        .catch((errors) => {
-          setResult([-1, -1, -1]);
-        });
-    };
+		fetchData();
+	}, [domain]);
 
-    fetchData();
-  }, [domain]);
+	return (
+		<div className="flex flex-col h-screen bg-white">
+			<NavBar />
+			<Head>
+				<title>DNS Comparison</title>
+				<link
+					rel="icon"
+					href="https://www.globalcyberalliance.org/wp-content/uploads/favicon.png"
+				/>
+			</Head>
 
-  return (
-    <div className="flex flex-col h-screen bg-white">
-      <NavBar />
-      <Head>
-        <title>DNS Comparison</title>
-        <link
-          rel="icon"
-          href="https://www.globalcyberalliance.org/wp-content/uploads/favicon.png"
-        />
-      </Head>
+			<main className="flex-grow p-8 mx-auto">
+				<h1 className="text-5xl font-bold text-center title">
+					DNS Comparison
+				</h1>
 
-      <main className="flex-grow p-8 mx-auto">
-        <h1 className="text-5xl font-bold text-center title">DNS Comparison</h1>
+				<p className="mb-6 text-xl text-center text-gray-700 description">
+					Enter a domain in the textbox below to compare it on
+					different DNS providers
+				</p>
 
-        <p className="mb-6 text-xl text-center text-gray-700 description">
-          Enter a domain in the textbox below to compare it on different DNS
-          providers
-        </p>
-
-        <form className="text-center">
-          <input
-            className={`bg-gray-200 appearance-none border-2 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white `}
-            type="text"
-            onChange={handleChange}
-          />
-        </form>
-        <Provider index="0" logo="./cloudflare.svg" />
-        <Provider index="1" logo="./google.svg" />
-        <Provider index="2" logo="./quad9.svg" />
-      </main>
-    </div>
-  );
+				<form className="text-center">
+					<input
+						className={
+							"bg-gray-200 appearance-none border-2 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white "
+						}
+						type="text"
+						onChange={handleChange}
+					/>
+				</form>
+				<Provider index="0" logo="./cloudflare.svg" />
+				<Provider index="1" logo="./google.svg" />
+				<Provider index="2" logo="./quad9.svg" />
+			</main>
+		</div>
+	);
 }
