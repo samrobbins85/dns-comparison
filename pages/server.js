7cdfeb44817dbfb00da6d9cb483b8049f3a148b4
Sam Robbins <samrobbinsgb@gmail.com>
2020-09-03 10:26:04 +0100

    Add comparison for all

--- pages/server.js
+++ pages/server.js
@@ -2,16 +2,73 @@ import Head from "next/head";
 import NavBar from "../components/navbar";
 import Papa from "papaparse";
 import { useState } from "react";
+import dynamic from "next/dynamic";
+import { useEffect } from "react";
+const Bar = dynamic(() => import("react-chartjs-2").then((mod) => mod.Bar));
+
 export default function Server() {
 	const [fileName, setFileName] = useState("Select a File");
+	const [fileOutput, setFileOutput] = useState(false);
+	const [chart, setChart] = useState({
+		labels: [
+			"Quad 9",
+			"Quad 9 Noblock",
+			"Google",
+			"Cloudflare Safe",
+			"Cloudflare",
+			"OpenDNS",
+			"Ultra Recursive",
+		],
+
+		datasets: [
+			{
+				label: "Blocks",
+				backgroundColor: "rgba(255,99,132,0.2)",
+				borderColor: "rgba(255,99,132,1)",
+				borderWidth: 1,
+				hoverBackgroundColor: "rgba(255,99,132,0.4)",
+				hoverBorderColor: "rgba(255,99,132,1)",
+				data: [0, 0, 0, 0, 0, 0, 0],
+			},
+		],
+	});
+
+	useEffect(() => {
+		if (fileOutput) {
+			let base = { ...chart };
+			base.datasets[0].data = [
+				fileOutput.data[1][1],
+				fileOutput.data[2][1],
+				fileOutput.data[3][1],
+				fileOutput.data[4][1],
+				fileOutput.data[5][1],
+				fileOutput.data[6][1],
+				fileOutput.data[7][1],
+			];
+			setChart(base);
+		}
+	}, [fileOutput]);
+
 	function FileChange(event) {
 		setFileName(event.target.files[0].name);
 		Papa.parse(event.target.files[0], {
 			complete: function (results) {
-				console.log(results);
+				setFileOutput(results);
 			},
 		});
 	}
+	const scale = {
+		maintainAspectRatio: true,
+		scales: {
+			yAxes: [
+				{
+					ticks: {
+						beginAtZero: true,
+					},
+				},
+			],
+		},
+	};
 	return (
 		<>
 			<Head>
@@ -31,7 +88,7 @@ export default function Server() {
 					Server Generated Results
 				</h1>
 				<div className="flex justify-center">
-					<label class="w-64 flex flex-col items-center px-2 py-2 bg-white text-gray-800 rounded-lg shadow  border border-gray-300 cursor-pointer hover:bg-gray-200">
+					<label className="w-64 flex flex-col items-center px-2 py-2 bg-white text-gray-800 rounded-lg shadow  border border-gray-300 cursor-pointer hover:bg-gray-200">
 						<svg
 							className="w-8 h-8"
 							fill="currentColor"
@@ -40,15 +97,26 @@ export default function Server() {
 						>
 							<path d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z" />
 						</svg>
-						<span class="mt-2 text-base leading-normal">
+						<span className="mt-2 text-base leading-normal">
 							{fileName}
 						</span>
 						<input
 							type="file"
-							class="hidden"
+							className="hidden"
 							onChange={FileChange}
 						/>
 					</label>
+				</div>
+				<div className="text-center pt-10 text-lg">
+					<h2>Total number of domains:</h2>
+					<span className="font-semibold text-xl">
+						{fileOutput && fileOutput.data[0][1]}
+					</span>
+					{fileOutput && (
+						<div className="container mx-auto">
+							<Bar data={chart} options={scale} height={100} />
+						</div>
+					)}
 				</div>
 			</div>
 		</>
